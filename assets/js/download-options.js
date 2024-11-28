31b83da0574aae85b3e0f18715e5c4daea0e6eaf
mohreh <mrezahassani@protonmail.com>
2024-11-28 20:10:50 +0330

    fix bugs

--- assets/js/download-options.js
+++ assets/js/download-options.js
@@ -1,119 +1,119 @@
 document.addEventListener("DOMContentLoaded", function () {
-	const downloadOptions = {
-		windows: [
-			{
-				name: ".exe",
-				desc: "بسته مناسب برای ویندوز 64 بیتی",
-				url: "./downloads/rdesk-1.2.4-install.exe",
-				arch: "x86_64",
-			},
-			{
-				name: ".exe",
-				desc: "بسته مناسب برای ویندوز ۳۲ بیتی",
-				url: "./downloads/rdesk-sciter-1.2.4-x86.exe",
-				arch: "x86",
-			},
-		],
-		mac: [
-			{
-				name: ".dmg",
-				desc: "بسته مناسب برای پردازنده‌های اینتل",
-				url: "./downloads/rdesk-1.2.4-x64-x86_64.dmg",
-				arch: "x86_64",
-			},
-			{
-				name: ".dmg",
-				desc: "بسته مناسب برای پردازنده‌های اپل",
-				url: "./downloads/rdesk-sciter-1.2.4-amd64.dmg",
-				arch: "arm64",
-			},
-			{
-				name: ".tar.xz",
-				desc: "بسته مناسب برای پردازنده‌های اپل",
-				url: "./downloads/rdesk-1.2.4-arm64.pkg.tar.xz",
-				arch: "arm64",
-			},
-		],
-		linux: [
-			{
-				name: ".AppImage",
-				desc: "بسته AppImage برای معماری x86_64",
-				url: "./downloads/rdesk-1.2.4-x86_64.AppImage",
-				arch: "x86_64",
-			},
-			{
-				name: ".AppImage",
-				desc: "بسته AppImage برای معماری arm64",
-				url: "./downloads/rdesk-1.2.4-arm64.AppImage",
-				arch: "arm64",
-			},
-			{
-				name: ".deb",
-				desc: "معماری x86_64 برای دبیان، اوبونتو و...",
-				url: "./downloads/rdesk-1.2.4-x86_64.deb",
-				arch: "x86_64",
-			},
-			{
-				name: ".deb",
-				desc: "معماری arm64 برای دبیان، اوبونتو و...",
-				url: "./downloads/rdesk-1.2.4-arm64.deb",
-				arch: "arm64",
-			},
-			{
-				name: ".rpm",
-				desc: "معماری x86_64 برای فدورا، ردهت و...",
-				url: "./downloads/rdesk-1.2.4-x86_64.rpm",
-				arch: "x86_64",
-			},
-			{
-				name: ".rpm",
-				desc: "معماری arm64 برای فدورا، ردهت و...",
-				url: "./downloads/rdesk-1.2.4-arm64.rpm",
-				arch: "arm64",
-			},
-			{
-				name: ".rpm",
-				desc: "معماری x86_64 برای SUSE",
-				url: "./downloads/rdesk-suse-1.2.4-x86_64.rpm",
-				arch: "x86_64",
-			},
-			{
-				name: ".rpm",
-				desc: "معماری arm64 برای SUSE",
-				url: "./downloads/rdesk-suse-1.2.4-0-arm64.rpm",
-				arch: "arm64",
-			},
-			{
-				name: ".flatpak",
-				desc: "بسته flatpak برای معماری x86_64",
-				url: "./downloads/rdesk-1.2.4-x86-64.flatpak",
-				arch: "x86_64",
-			},
-		],
-		android: [
-			{
-				name: ".apk",
-				desc: "معماری x86_64 برای اندروید",
-				url: "./downloads/rdesk-1.2.4-x86_64.apk",
-				arch: "x86_64",
-			},
-			{
-				name: ".apk",
-				desc: "معماری aarch64 برای اندروید",
-				url: "./downloads/rdesk-1.2.4-aarch64.apk",
-				arch: "aarch64",
-			},
-			{
-				name: ".apk",
-				desc: "معماری armv7 برای اندروید",
-				url: "./downloads/rdesk-1.2.4-armv7.apk",
-				arch: "armv7",
-			},
-		],
-	};
+  const downloadOptions = {
+    windows: [
+      {
+        name: ".exe",
+        desc: "بسته مناسب برای ویندوز 64 بیتی",
+        url: "./downloads/rdesk-1.2.4-install.exe",
+        arch: "x86_64",
+      },
+      {
+        name: ".exe",
+        desc: "بسته مناسب برای ویندوز ۳۲ بیتی",
+        url: "./downloads/rdesk-sciter-1.2.4-x86.exe",
+        arch: "x86",
+      },
+    ],
+    mac: [
+      {
+        name: ".dmg",
+        desc: "بسته مناسب برای پردازنده‌های اینتل",
+        url: "./downloads/rdesk-1.2.4-x64-x86_64.dmg",
+        arch: "x86_64",
+      },
+      {
+        name: ".dmg",
+        desc: "بسته مناسب برای پردازنده‌های اپل",
+        url: "./downloads/rdesk-sciter-1.2.4-amd64.dmg",
+        arch: "arm64",
+      },
+      {
+        name: ".tar.xz",
+        desc: "بسته مناسب برای پردازنده‌های اپل",
+        url: "./downloads/rdesk-1.2.4-arm64.pkg.tar.xz",
+        arch: "arm64",
+      },
+    ],
+    linux: [
+      {
+        name: ".AppImage",
+        desc: "بسته AppImage برای معماری x86_64",
+        url: "./downloads/rdesk-1.2.4-x86_64.AppImage",
+        arch: "x86_64",
+      },
+      {
+        name: ".AppImage",
+        desc: "بسته AppImage برای معماری arm64",
+        url: "./downloads/rdesk-1.2.4-arm64.AppImage",
+        arch: "arm64",
+      },
+      {
+        name: ".deb",
+        desc: "معماری x86_64 برای دبیان، اوبونتو و...",
+        url: "./downloads/rdesk-1.2.4-x86_64.deb",
+        arch: "x86_64",
+      },
+      {
+        name: ".deb",
+        desc: "معماری arm64 برای دبیان، اوبونتو و...",
+        url: "./downloads/rdesk-1.2.4-arm64.deb",
+        arch: "arm64",
+      },
+      {
+        name: ".rpm",
+        desc: "معماری x86_64 برای فدورا، ردهت و...",
+        url: "./downloads/rdesk-1.2.4-x86_64.rpm",
+        arch: "x86_64",
+      },
+      {
+        name: ".rpm",
+        desc: "معماری arm64 برای فدورا، ردهت و...",
+        url: "./downloads/rdesk-1.2.4-arm64.rpm",
+        arch: "arm64",
+      },
+      {
+        name: ".rpm",
+        desc: "معماری x86_64 برای SUSE",
+        url: "./downloads/rdesk-suse-1.2.4-x86_64.rpm",
+        arch: "x86_64",
+      },
+      {
+        name: ".rpm",
+        desc: "معماری arm64 برای SUSE",
+        url: "./downloads/rdesk-suse-1.2.4-0-arm64.rpm",
+        arch: "arm64",
+      },
+      {
+        name: ".flatpak",
+        desc: "بسته flatpak برای معماری x86_64",
+        url: "./downloads/rdesk-1.2.4-x86-64.flatpak",
+        arch: "x86_64",
+      },
+    ],
+    android: [
+      {
+        name: ".apk",
+        desc: "معماری x86_64 برای اندروید",
+        url: "./downloads/rdesk-1.2.4-x86_64.apk",
+        arch: "x86_64",
+      },
+      {
+        name: ".apk",
+        desc: "معماری aarch64 برای اندروید",
+        url: "./downloads/rdesk-1.2.4-aarch64.apk",
+        arch: "aarch64",
+      },
+      {
+        name: ".apk",
+        desc: "معماری armv7 برای اندروید",
+        url: "./downloads/rdesk-1.2.4-armv7.apk",
+        arch: "armv7",
+      },
+    ],
+  };
 
-	function createButton(option) {
-		return `
+  function createButton(option) {
+    return `
       <a href="${option.url}" class="btn btn-md btn-orange-red m-1 text-center tra-white-hover">
         <div class="ico-20">
           <img src="assets/images/static/download.svg" class="img-fluid white-color" alt="Download icon">
@@ -122,46 +122,62 @@ document.addEventListener("DOMContentLoaded", function () {
         <p class="tra-white-hover p-xs white-color">${option.desc}</p>
       </a>
     `;
-	}
+  }
 
-	function showDownloadOptionsTop(os, arch) {
-		const downloadSectionTop = document.getElementById("download-top");
-		downloadSectionTop.insertAdjacentHTML(
-			"afterbegin",
-			`<img src="assets/images/static/${os}.svg" class="ico-35 img-fluid">`,
-		);
+  function showDownloadOptionsTop(os, arch) {
+    const downloadSectionTop = document.getElementById("download-top");
+    downloadSectionTop.insertAdjacentHTML(
+      "afterbegin",
+      `<img src="assets/images/static/${os}.svg" class="ico-35 img-fluid">`,
+    );
 
-		const optionsContainer = document.getElementById("download-options-top");
-		downloadOptions[os].forEach((option) => {
-			if (option.arch === arch) {
-				optionsContainer.insertAdjacentHTML("afterbegin", createButton(option));
-			}
-		});
-	}
+    const optionsContainer = document.getElementById("download-options-top");
+    let added = false;
+    downloadOptions[os]?.forEach((option) => {
+      if (arch === undefined) {
+        optionsContainer.insertAdjacentHTML("afterbegin", createButton(option));
+        added = true;
+      } else if (option.arch === arch) {
+        optionsContainer.insertAdjacentHTML("afterbegin", createButton(option));
+        added = true;
+      }
+    });
 
-	function showDownloadOptions(os) {
-		const optionsContainer = document.getElementById("download-options");
+    if (!added) {
+      downloadSectionTop.innerHTML = `
+        <a href="#download"
+                                                class="btn btn-md btn-orange-red tra-white-hover mr-15">سریع‌ترین
+                                                نرم‌افزار ریموت
+                                                دسکتاپ را دانلود کنید</a>
+        `;
+      optionsContainer.style.display = "none";
+    }
+  }
 
-		downloadOptions[os].forEach((option) => {
-			optionsContainer.insertAdjacentHTML("beforeend", createButton(option));
-		});
+  function showDownloadOptions(os) {
+    const optionsContainer = document.getElementById("download-options");
+    optionsContainer.innerHTML = "";
 
-		document
-			.querySelectorAll(".os-selection img")
-			.forEach((btn) => btn.classList.remove("active"));
-		document
-			.querySelector(`.os-selection img[data-os="${os}"]`)
-			.classList.add("active");
-	}
+    downloadOptions[os].forEach((option) => {
+      optionsContainer.insertAdjacentHTML("beforeend", createButton(option));
+    });
 
-	const { os, arch } = detectUserOS();
+    document
+      .querySelectorAll(".os-selection img")
+      .forEach((btn) => btn.classList.remove("active"));
+    document
+      .querySelector(`.os-selection img[data-os="${os}"]`)
+      .classList.add("active");
+  }
 
-	showDownloadOptions(os);
-	showDownloadOptionsTop(os, arch);
+  const { os, arch } = detectUserOS();
 
-	document.querySelectorAll(".os-selection img").forEach((button) => {
-		button.addEventListener("click", function () {
-			showDownloadOptions(this.dataset.os);
-		});
-	});
+  showDownloadOptions(os);
+  showDownloadOptionsTop(os, arch);
+
+  document.querySelectorAll(".os-selection img").forEach((button) => {
+    button.addEventListener("click", function () {
+      showDownloadOptions(this.dataset.os);
+    });
+  });
 });
--- assets/js/platform-detection.js
+++ assets/js/platform-detection.js
@@ -1,56 +1,72 @@
+function containsAny(str, substringsToContain, substringsToExclude = []) {
+  return (
+    substringsToContain.some((substring) => str.includes(substring)) &&
+    !substringsToExclude.some((substring) => str.includes(substring))
+  );
+}
+
+function containsAll(str, substrings) {
+  return substrings.every((substring) => str.includes(substring));
+}
+
+function detectArch(os, agent) {
+  console.log(os, agent);
+  let architecture;
+  if (containsAny(agent, ["64", "long"], ["arm", "aarch"])) {
+    // x86_64, x86-64, Intel 64, EM64T, 64-bit x86, ia-64, Long Mode and amd64
+    architecture = "x86_64";
+  } else if (containsAny(agent, ["iphone", "ipad", "ipod"])) {
+    os = "ios";
+    architecture = "arm64";
+  } else if (containsAny(agent, ["32", "86"])) {
+    // IA-32, 80*86, x86, i386, i486, i586, i686
+    architecture = "x86";
+
+    if (os == "android") {
+      architecture = "x86_64";
+    }
+  } else if (containsAny(agent, ["arm", "aarch"])) {
+    if (containsAny(agent, ["32", "v7", "v8-r", "v8-m", "hf", "eabi", "el"])) {
+      architecture = "armv7";
+    } else if (containsAny(agent, ["64"])) {
+      if (os == "android") {
+        architecture = "aarch64";
+      } else {
+        architecture = "arm64";
+      }
+    }
+  } else {
+    architecture = undefined;
+  }
+  return architecture;
+}
+
 function detectUserOS() {
-    const userAgent = navigator.userAgent.toLowerCase();
+  const userAgent = navigator.userAgent.toLowerCase();
 
-    function containsAny(str, substringsToContain, substringsToExclude = []) {
-        return (
-            substringsToContain.some((substring) => str.includes(substring)) &&
-            !substringsToExclude.some((substring) => str.includes(substring))
-        );
+  let os;
+  if (containsAny(userAgent, ["win"])) {
+    os = "windows";
+  } else if (containsAny(userAgent, ["mac", "os x", "macintosh"])) {
+    os = "mac";
+  } else if (containsAny(userAgent, ["android"])) {
+    os = "android";
+  } else if (containsAny(userAgent, ["linux", "x11"])) {
+    os = "linux";
+  } else {
+    os = "windows";
+  }
+
+  let architecture = detectArch(os, userAgent);
+  if ((architecture === undefined) & (os === "android")) {
+    let agent = navigator.platform.toLowerCase();
+    if (agent) {
+      architecture = detectArch(os, agent);
     }
+  }
 
-    function containsAll(str, substrings) {
-        return substrings.every((substring) => str.includes(substring));
-    }
-
-    let os;
-    console.log(userAgent);
-    if (containsAny(userAgent, ["win"])) {
-        os = "windows";
-    } else if (containsAny(userAgent, ["mac"])) {
-        os = "mac";
-    } else if (containsAny(userAgent, ["linux", "x11"])) {
-        os = "linux";
-    } else if (containsAny(userAgent, ["andoird"])) {
-        os = "android";
-    } else {
-        os = "windows";
-    }
-
-    let architecture;
-    if (containsAny(userAgent, ["64", "long"], ["arm", "a64"])) {
-        // x86_64, x86-64, Intel 64, EM64T, 64-bit x86, ia-64, Long Mode and amd64
-        architecture = "x86_64";
-    } else if (containsAny(userAgent, ["32", "86"])) {
-        // IA-32, 80*86, x86, i386, i486, i586, i686
-        architecture = "x86";
-    } else if (containsAny(userAgent, ["arm", "aarch"])) {
-        if (
-            containsAny(userAgent, ["32", "v7", "v8-r", "v8-m", "hf", "eabi", "el"])
-        ) {
-            architecture = "armv7";
-        } else if (containsAny(userAgent, ["64"])) {
-            if (os == "android") {
-                architecture = "aarch64";
-            } else {
-                architecture = "arm64";
-            }
-        }
-    } else {
-        architecture = undefined;
-    }
-
-    return {
-        os: os,
-        arch: architecture,
-    };
+  return {
+    os: os,
+    arch: architecture,
+  };
 }
